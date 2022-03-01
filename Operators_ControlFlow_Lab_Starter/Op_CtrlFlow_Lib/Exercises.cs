3cc4b69fbd660e6066542fbad2ca1924d3fa06dd
Marian Dumitriu <md1246@live.mdx.ac.uk>
2022-03-01 00:10:06 +0000

    Completed OP_CRT Flow exercise
    Completed operation control flow exercise, including guessing what MyMethod did,
    return the average sum of a list,
    completed ticket system and passed all test cases, 
    Created Grading system and added relevant test cases
    and created covid Scottish wedding restriction using the switch statement.

--- Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
+++ Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Op_CtrlFlow
 {
@@ -7,12 +8,21 @@ namespace Op_CtrlFlow
         public static bool MyMethod(int num1, int num2)
         {
             return num1 == num2 ? false : (num1 % num2) == 0;
+            //if the 2 numbers are diffrent check to see if num 2 is a factor of num 1
         }
 
         // returns the average of the array nums
         public static double Average(List<int> nums)
         {
-            return -1;
+            if (nums.Count == 0)
+                return 0;
+            
+            double sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            return sum / nums.Count;
         }
 
         // returns the type of ticket a customer is eligible for based on their age
@@ -23,19 +33,79 @@ namespace Op_CtrlFlow
         // "Free" if they are under 5
         public static string TicketType(int age)
         {
-            string ticketType = string.Empty;
-            return ticketType;
+            if (5 > age && age >= 0)
+                return "Free";
+
+            else if (13 > age && age >= 5)
+                return "Child";
+
+            else if (18 > age && age >= 13)
+                return "Student";
+
+            if (60 > age && age >= 18)
+                return "Standard";
+
+            else
+                return "OAP";
         }
 
+
+        /*  | 75 to 100 | "Pass with Distinction" |
+            | --------: | ----------------------- |
+            |  60 to 74 | "Pass with Merit"       |
+            |  40 to 59 | "Pass"                  |
+            |   0 to 39 | "Fail"                  |  */
         public static string Grade(int mark)
         {
-            var grade = "";
-            return grade;
-        }
+            if (0 > mark || mark > 100)
+                return "Parameter out of bounds";
+                //throw new ArgumentException("Parameter out of bounds");
 
+            else if (75 <= mark)
+                return "Pass with Distinction";
+
+            else if (60 <= mark)
+                return "Pass with Merit";
+
+            else if (40 <= mark)
+                return "Pass";
+
+            else return "Fail";
+
+            //return 0 > mark || mark > 100 ? "Pareneter out of bounds" :
+            //    75 <= mark ? "Pass with Distinction" : 60 <= mark ? "Pass with Merit" :
+            //    40 <= mark ? "Pass" : "Fail";
+         }
+
+        /*  | Level | Max attendees |
+            | ----: | ------------- |
+            |     4 | 20            |
+            |     3 | 50            |
+            |     2 | 50            |
+            |     1 | 100           |
+            |     0 | 200           |  */
         public static int GetScottishMaxWeddingNumbers(int covidLevel)
         {
-            return 0;
+            switch (covidLevel) 
+            {
+                case 4:
+                    return 20;
+                case 3:
+                case 2:
+                    return 50;
+                case 1:
+                    return 100;
+                case 0:
+                    return 200;
+                default:
+                    return 0;
+
+                //4 => 20,
+                //3 or 2 => 50,
+                //1 => 100,
+                //0 => 200,
+                //_ => 0,
+            }
         }
     }
 }
--- Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
+++ Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
@@ -5,9 +5,29 @@ using System.Collections.Generic;
 namespace Op_CtrlFlow_Tests
 {
     public class Exercises_Tests
-    {     
+    {
         // write unit test(s) for MyMethod here
+        // Num1 // factor // isfactor
+        [Category("Every number is a factor of 0!")]
+        [TestCase(0, 1, true)]
+        [TestCase(0, -1, true)]
+        [TestCase(0, 12, true)]
+        [TestCase(0, -12, true)]
+        [TestCase(0, 1992992, true)]
+        [TestCase(0, -1992992, true)]
 
+        [Category("Who are the factors of 6")] //1,2,3,6
+        [TestCase(6, 2, true)]
+        [TestCase(6, -2, true)]
+        [TestCase(6, 3, true)]
+        [TestCase(6, -3, true)]
+        [TestCase(6, 4, false)]
+        [TestCase(6, -4, false)]
+        //[TestCase(2, 0, false)] //div by 0 error
+        public void IsNum2FactorOfNum2_MyMethod_ReturnsCorrectAverage(int a, int b, bool expected)
+        {
+            Assert.That(Exercises.MyMethod(a,b), Is.EqualTo(expected));
+        }
 
         [Test]
         public void Average_ReturnsCorrectAverage()
@@ -37,6 +57,31 @@ namespace Op_CtrlFlow_Tests
         {
             var result = Exercises.TicketType(age);
             Assert.That(result, Is.EqualTo(expected));
+        }
+
+
+        [Category("Out of bounds")]
+        [TestCase(101, "Parameter out of bounds")]
+        [TestCase(-1, "Parameter out of bounds")]
+
+        [Category("Edge Case")]
+        [TestCase(100, "Pass with Distinction")]
+        [TestCase(75, "Pass with Distinction")]
+        [TestCase(74, "Pass with Merit")]
+        [TestCase(60, "Pass with Merit")]
+        [TestCase(59, "Pass")]
+        [TestCase(40, "Pass")]
+        [TestCase(39, "Fail")]
+        [TestCase(0, "Fail")]
+
+        [Category("Safe Case")]
+        [TestCase(85, "Pass with Distinction")]
+        [TestCase(70, "Pass with Merit")]
+        [TestCase(50, "Pass")]
+        [TestCase(25, "Fail")]
+        public void GradeTest(int grade, string expected)
+        {
+            Assert.That(expected, Is.EqualTo(Exercises.Grade(grade)));
         }
     }
 }
